cfb992051eb13a31a8de31d01413ec22e6e1ab2d
Aleke <alnur.rysbek@mail.ru>
2022-11-04 09:29:15 +0600

    success

--- src/main/java/school/mjc/stage0/loops/task3/GreatestCommonDivisor.java
+++ src/main/java/school/mjc/stage0/loops/task3/GreatestCommonDivisor.java
@@ -2,5 +2,26 @@ package school.mjc.stage0.loops.task3;
 
 public class GreatestCommonDivisor {
     public void printGCD(int first, int second) {
+        int max = 0;
+        if(first>second){
+            for(int i=2;i<=first;i++){
+                if(first%i==0 && second%i==0){
+                    max = i;
+                }
+            }
+        }else if(second>first){
+            for(int i=2;i<=second;i++){
+                if(first%i==0 && second%i==0){
+                    max = i;
+                }
+            }
+        }else{
+            for(int i=2;i<=first;i++){
+                if(first%i==0 && second%i==0){
+                    max = i;
+                }
+            }
+        }
+        System.out.println(max);
     }
 }
--- src/main/java/school/mjc/stage0/loops/task3/NinesSum.java
+++ src/main/java/school/mjc/stage0/loops/task3/NinesSum.java
@@ -2,6 +2,14 @@ package school.mjc.stage0.loops.task3;
 
 public class NinesSum {
     public void calculateSum(int lengthOfLastNumber) {
-
+        int pow =1;
+        int total = 0;
+        int count  =0;
+        for(int i = 0;i<lengthOfLastNumber;i++){
+            pow *=10;
+            total = pow-1;
+            count = count+total;
+        }
+        System.out.println(count);
     }
 }
